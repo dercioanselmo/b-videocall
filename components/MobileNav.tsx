7d614a576c29946de38adcb4a2240e9272ca797f
Dercio Anselmo <dercio@branchdev.io>
2025-07-03 21:12:44 +0400

    Implemented the top part of the home page

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -29,7 +29,7 @@ const MobileNav = () => {
                     alt='b-Videocall logo'
                     className='max:sm:size-10' 
                     />
-                    <p className='text-[26px] font-extrabold text-white'>b-Videocall</p>
+                    <p className='text-[26px] font-extrabold text-white'>b-VideoCall</p>
                 </Link>
                 <div className="=flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
                     <SheetClose asChild>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
           alt='b-Videocall logo'
           className='max:sm:size-10' 
         />
-        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>b-Videocall</p>
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>b-VideoCall</p>
       </Link>
       <div className='flex-between gap-5'>
         <SignedIn>
