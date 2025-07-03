eab4b1fd696e47674f82c97b2a7443cc49da986c
Dercio Anselmo <dercio@branchdev.io>
2025-07-03 16:25:49 +0400

    Responsiveness, layout and routing done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
   return (
-    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+    <nav className='flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
       <Link href="/" className='flex items-center gap-1'>
         <Image
           src="/icons/logo.svg"
@@ -15,6 +16,10 @@ const Navbar = () => {
         />
         <p className='text-[26px] font-extrabold text-white max-sm:hidden'>b-Videocall</p>
       </Link>
+      <div className='flex-between gap-5'>
+        {/* Clerk - User Management */}
+        <MobileNav />
+      </div>
     </nav>
   )
 }
